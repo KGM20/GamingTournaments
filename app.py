92d5072dc04ffe26eea8f9a90768f46df019b014
Kevin Cruz <kevine.cruzd@hotmail.com>
2020-01-31 12:44:43 -0600

    feat: error handlers for most common cases (404, 405, 422, 500)

--- app.py
+++ app.py
@@ -10,6 +10,38 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
 
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'Not found'
+        }), 404
+
+    @app.errorhandler(405)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 405,
+            'message': 'Method not allowed'
+        }), 405
+
+    @app.errorhandler(422)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': 'Unprocessable entity'
+        }), 422
+
+    @app.errorhandler(500)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'Internal server error'
+        }), 500
+
     return app
 
 app = create_app()
