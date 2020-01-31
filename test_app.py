086c472847491ff0dc3390b4ee1d4a0d567912ba
Kevin Cruz <kevine.cruzd@hotmail.com>
2020-01-30 23:21:19 -0600

    test: create a database and set it for testing environment

--- test_app.py
+++ test_app.py
@@ -1,1 +1,37 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
 
+from app import create_app
+from models import setup_db, Player, Game, Tourney
+
+
+class GamingTourneyTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "test.db"
+        self.project_dir = os.path.dirname(os.path.abspath(__file__))
+        self.database_path = "sqlite:///{}".format(os.path.join(self.project_dir,
+                                                  				self.database_filename))
+        setup_db(self.app, self.database_path)
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+    def tearDown(self):
+        """Executed after each test"""
+        pass
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
