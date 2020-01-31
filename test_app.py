e9b52cf859d28626bda1becf629a91af28221445
Kevin Cruz <kevine.cruzd@hotmail.com>
2020-01-31 14:50:23 -0600

    test: POST /players route

--- test_app.py
+++ test_app.py
@@ -20,6 +20,18 @@ class GamingTourneyTestCase(unittest.TestCase):
                                                   				self.database_name))
         setup_db(self.app, self.database_path)
 
+        self.new_player = {
+            'name': 'A real name',
+            'nickname': 'A cool nickname',
+            'nationality': 'Some nice country'
+        }
+
+        self.wrong_datatypes_player = {
+            'name': 'A real name',
+            'nickname': 123456,
+            'nationality': 'Some nice country'
+        }
+
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
@@ -46,6 +58,22 @@ class GamingTourneyTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 405)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'Method not allowed')
+
+    def test_create_new_player(self):
+        res = self.client().post('/players', json=self.new_player)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_422_create_player_with_wrong_datatype(self):
+        res = self.client().post('/players',
+                                 json=self.wrong_datatypes_player)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable entity')
 
 
 # Make the tests conveniently executable
