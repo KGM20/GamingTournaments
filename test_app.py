167937157b01b6cd22e9e3d59dbef44292c8740b
Kevin Cruz <kevine.cruzd@hotmail.com>
2020-01-31 21:15:51 -0600

    test: DELETE /games route

--- test_app.py
+++ test_app.py
@@ -156,6 +156,21 @@ class GamingTourneyTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'Unprocessable entity')
 
+    def test_delete_game(self):
+        res = self.client().delete('/games/5')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_404_delete_a_non_existing_game(self):
+        res = self.client().delete('/games/1234567890')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not found')
+
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
